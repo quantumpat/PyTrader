fb0b6891adfe6e9812e48b34b091323dfd920f72
Patrick51090 <patrick.carroll715@gmail.com>
2023-02-21 20:51:38 -0500

    Basic RSI calculation

--- dev/indicators/rsi.py
+++ dev/indicators/rsi.py
@@ -5,13 +5,10 @@ import datetime
 
 
 
-def get_rsi(ticker):
-    stock_data = yfinance.download(ticker, interval="1m", start=str(datetime.date.today() - datetime.timedelta(5)),
-                                   end=str(datetime.date.today()))
+def get_rsi(ticker, last=True):
+    stock_data = yfinance.download(ticker, interval="1m", start=str(datetime.date.today() - datetime.timedelta(5)),end=str(datetime.date.today()))
 
     total_rsi = ta.rsi(stock_data["Close"], length=14)
-
-
 
     class rsiData:
         def __init__(self, rsi):
