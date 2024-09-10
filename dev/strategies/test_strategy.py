abf5eb04da19a3f61c923eca2d233f684cfe4368
quantumpat <patrickjcarroll17@gmail.com>
2024-09-10 15:03:48 -0600

    Added Strategy

---
+++ dev/strategies/test_strategy.py
@@ -0,0 +1,5 @@
+
+from dev.indicators.adx import get_adx
+from dev.indicators.rsi import get_rsi
+
+print("Hello World");
--- dev/sys/view_indicators.py
+++ dev/sys/view_indicators.py
@@ -1,7 +1,7 @@
 from dev.indicators.adx import get_adx
 from dev.indicators.rsi import get_rsi
 
-ticker = 'AAPL'
+ticker = 'NVDA'
 
 print(f'RSI: {get_rsi(ticker).last_rsi}')
 
